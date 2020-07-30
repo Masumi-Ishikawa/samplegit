648232fcbc61e0873176004fd81919b8cde5e679
aa10 <aa10@DESKTOP-U4GS1M5>
2020-07-30 17:47:15 +0900

    文言修正

--- SampleHello/src/Hello.java
+++ SampleHello/src/Hello.java
@@ -4,13 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 
 		System.out.println("はじめまして");
-		System.out.println("こんにちは");
-		System.out.println("よろしくお願いします");
 
-	}
-
-	public void test() {
-		System.out.println("テストですよー");
 	}
 
 }
